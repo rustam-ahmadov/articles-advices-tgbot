042486cee5b57dc773db01c531882d2c8e3d7042
rustam-ahmadov <ahmadov.rustam114@gmail.com>
2024-10-28 11:11:12 +0400

    feat: client

--- main.go
+++ main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"flag"
+	"log"
+	"m/client/telegram"
+)
+
+const (
+	tgBotHost = "api.telegram.org"
+)
+
+func main() {
+	tgClient := telegram.New(tgBotHost, mustToken())
+
+}
+
+func mustToken() string {
+	token := flag.String("bot-token", "", "tg-bot access token")
+
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatalf("token has not been provided")
+	}
+
+	return *token
+}
